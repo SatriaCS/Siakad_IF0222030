bf74e27235f462461e55658127349b425bc36450
SatriaCS <cahayasakit@gmail.com>
2024-05-30 17:00:05 +0700

    Install Bootstrap

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/beban', function () {
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
